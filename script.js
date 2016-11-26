14e74977394d403ed8429d0f91320494fa819e02
thillak <thillak.sachi@gmail.com>
2016-11-25 18:28:41 -0600

    W

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
  function askuser() {
  	var answer="   "; // answer holder
  	var statement = "Right or wrong"; // input
- 	var answer = prompt("Who's  that pokemon? ( case sensetive)" ); //question
+ 	var answer = prompt("Who's  that pokemon? ( case sensitive)" ); //question
 
 
  	 if (answer == "psyduck") { //lowercaps
  	 	document.getElementById("poke").src = "New.png";
- 	 	statement ="It's PSYDUCK... you are correct!";	 	
+		statement = "It's DJ KHALED...You played yourself";	
+		document.getElementById("audioz").src= "win.mp3";
  	 }	
 
  	 else if (answer == "Psyduck") { //caps
  	 	document.getElementById("poke").src = "New.png";
- 	 	statement ="It's PSYDUCK... you are correct!";	 	
+ 		 statement = "It's DJ KHALED...You played yourself";	
+ 		 document.getElementById("audioz").src= "win.mp3"; 	
  	 }	
 
 
  	 else { //other answers
  	 	document.getElementById("poke").src = "New.png";
- 	 	statement = "It's PSYDUCK!... unfortunately you can't recognize our glorious leader lord psyduck";
+ 	 	statement = "It's DJ KHALED...You played yourself";
+ 	 	document.getElementById("audioz").src= "win.mp3";
  	 }
 
  	 alert(statement)
