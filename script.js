d6115832ddedecb5bd0ac81b7153632eeb1c5b7d
thillak <thillak.sachi@gmail.com>
2016-11-25 19:21:13 -0600

    Added a secret suprise :)

--- script.js
+++ script.js
@@ -4,23 +4,23 @@
  	var answer = prompt("Who's  that pokemon? ( case sensitive)" ); //question
 
 
- 	 if (answer == "psyduck") { //lowercaps
- 	 	document.getElementById("poke").src = "New.png";
-		statement = "It's DJ KHALED...You played yourself";	
+ 	 if (answer == "DJ KHALED") { //lowercaps
+ 	 	document.getElementById("poke").src = "psykhaled.png";
 		document.getElementById("audioz").src= "win.mp3";
+		statement = "It's DJ KHALED... YOU ARE CORRECT ";	
  	 }	
 
- 	 else if (answer == "Psyduck") { //caps
- 	 	document.getElementById("poke").src = "New.png";
- 		 statement = "It's DJ KHALED...You played yourself";	
- 		 document.getElementById("audioz").src= "win.mp3"; 	
+ 	 else if (answer == "Dj Khaled") { //caps
+ 	  	document.getElementById("poke").src = "psykhaled.png";	
+ 		document.getElementById("audioz").src= "win.mp3"; 	
+ 		statement = "It's DJ KHALED... YOU ARE CORRECT ";	
  	 }	
 
 
  	 else { //other answers
- 	 	document.getElementById("poke").src = "New.png";
- 	 	statement = "It's DJ KHALED...You played yourself";
+ 	 	document.getElementById("poke").src = "psykhaled.png";
  	 	document.getElementById("audioz").src= "win.mp3";
+ 	 	statement = "It's DJ KHALED...You played yourself";
  	 }
 
  	 alert(statement)
